d5e8e682cf10787114a7cffd880bc5a4cf4c587e
Zecharias Mengis <zmengis@gmail.com>
2021-03-16 14:12:17 -0500

    commit tree f9dbdae033b915b00ae5394f453036304bb75f3c

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    def initialize(name)
+        @name = name
+    end
+end
+
+# class Dog
+#     def initialize(breed)
+#       @breed = breed
+#     end
+  
+#     def breed=(breed)
+#       @breed = breed
+#     end
+  
+#     def breed
+#       @breed
+#     end
+#   end 
